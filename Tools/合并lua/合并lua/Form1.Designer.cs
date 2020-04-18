7c85e454c08c2b174ab106b0d7dd796469b38d21
gcud <gcudwork@163.com>
2020-04-18 12:55:24 +0800

    增加合并lua文件的工具
    仅能用来合并指定文件夹下的所有lua文件
    支持保存多个路径

---
+++ Tools/合并lua/合并lua/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace 合并lua
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PathComboBox = new System.Windows.Forms.ComboBox();
+            this.SaveConfigButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SavePathMaxTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ContentTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LastMergeLabel = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "目录";
+            // 
+            // PathComboBox
+            // 
+            this.PathComboBox.FormattingEnabled = true;
+            this.PathComboBox.Location = new System.Drawing.Point(47, 6);
+            this.PathComboBox.Name = "PathComboBox";
+            this.PathComboBox.Size = new System.Drawing.Size(636, 20);
+            this.PathComboBox.TabIndex = 1;
+            // 
+            // SaveConfigButton
+            // 
+            this.SaveConfigButton.Location = new System.Drawing.Point(12, 95);
+            this.SaveConfigButton.Name = "SaveConfigButton";
+            this.SaveConfigButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveConfigButton.TabIndex = 2;
+            this.SaveConfigButton.Text = "保存配置";
+            this.SaveConfigButton.UseVisualStyleBackColor = true;
+            this.SaveConfigButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "目录最大保存条数";
+            // 
+            // SavePathMaxTextBox
+            // 
+            this.SavePathMaxTextBox.Location = new System.Drawing.Point(121, 38);
+            this.SavePathMaxTextBox.Name = "SavePathMaxTextBox";
+            this.SavePathMaxTextBox.Size = new System.Drawing.Size(100, 21);
+            this.SavePathMaxTextBox.TabIndex = 4;
+            this.SavePathMaxTextBox.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0为无限制";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "开始合并";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ContentTextBox
+            // 
+            this.ContentTextBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ContentTextBox.Location = new System.Drawing.Point(0, 124);
+            this.ContentTextBox.Multiline = true;
+            this.ContentTextBox.Name = "ContentTextBox";
+            this.ContentTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ContentTextBox.Size = new System.Drawing.Size(800, 326);
+            this.ContentTextBox.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "复制内容";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "上次合并时间";
+            // 
+            // LastMergeLabel
+            // 
+            this.LastMergeLabel.AutoSize = true;
+            this.LastMergeLabel.Location = new System.Drawing.Point(100, 77);
+            this.LastMergeLabel.Name = "LastMergeLabel";
+            this.LastMergeLabel.Size = new System.Drawing.Size(17, 12);
+            this.LastMergeLabel.TabIndex = 10;
+            this.LastMergeLabel.Text = "无";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LastMergeLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ContentTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SavePathMaxTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SaveConfigButton);
+            this.Controls.Add(this.PathComboBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "合并lua";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox PathComboBox;
+        private System.Windows.Forms.Button SaveConfigButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SavePathMaxTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ContentTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LastMergeLabel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Tools/合并lua/合并lua/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+namespace 合并lua
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string PathListString = ReadConfig("PathList", "");
+            if (PathListString != "")
+            {
+                PathComboBox.Items.AddRange(PathListString.Split(','));
+            }
+            PathComboBox.Text = ReadConfig("NowPath", "");
+            SavePathMaxTextBox.Text = ReadConfig("SavePathMax", "10");
+            LastMergeLabel.Text = ReadConfig("LastMerge","无");
+        }
+        private string ReadConfig(string Key, string Default)
+        {
+            if (ConfigurationManager.AppSettings[Key] == null)
+            {
+                return Default;
+            }
+            else
+            {
+                return ConfigurationManager.AppSettings[Key];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddUniqueConfig("PathList", PathComboBox.Text, Int32.Parse(SavePathMaxTextBox.Text));
+            SaveConfig("SavePathMax", SavePathMaxTextBox.Text);
+            SaveConfig("NowPath", PathComboBox.Text);
+        }
+        private void AddUniqueConfig(string Key, string Value, int Max = 0)
+        {
+            if (ConfigurationManager.AppSettings[Key] == null)
+            {
+                AddConfig(Key, Value, Max);
+            }
+            else
+            {
+                string[] Configs = ConfigurationManager.AppSettings[Key].Split(',');
+                foreach (var item in Configs)
+                {
+                    if (item == Value)
+                    {
+                        return;
+                    }
+                }
+                AddConfig(Key, Value, Max);
+            }
+
+        }
+        private void AddConfig(string Key, string Value, int Max = 0)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings.Add(Key, Value);
+            if (Max > 0)
+            {
+                List<string> Configs = config.AppSettings.Settings[Key].Value.Split(',').ToList();
+                if (Configs.Count > Max)
+                {
+                    Configs.RemoveAt(0);
+                }
+                config.AppSettings.Settings[Key].Value = string.Join(",", Configs);
+            }
+
+            config.Save();
+        }
+        private void SaveConfig(string Key, string Value)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings[Key] == null)
+            {
+                config.AppSettings.Settings.Add(Key, Value);
+            }
+            else
+            {
+                config.AppSettings.Settings[Key].Value = Value;
+            }
+            config.Save();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string RootPath = PathComboBox.Text + Path.DirectorySeparatorChar;
+            if (Directory.Exists(RootPath))
+            {
+                string[] Files = Directory.GetFiles(RootPath, "*.lua", SearchOption.AllDirectories);
+                if (Files.Length > 0)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var item in Files)
+                    {
+                        sb.Append(File.ReadAllText(item));
+                        sb.Append(Environment.NewLine);
+                    }
+                    ContentTextBox.Text = sb.ToString();
+                }
+                else
+                {
+                    MessageBox.Show(PathComboBox.Text + " 没有lua文件");
+                }
+            }
+            else
+            {
+                MessageBox.Show(PathComboBox.Text + " 不存在");
+            }
+            SaveConfig("LastMerge",DateTime.Now.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(ContentTextBox.Text);
+        }
+    }
+}
